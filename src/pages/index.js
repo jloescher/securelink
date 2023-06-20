74b88d698146fa09cd6de5d7e2fcafe1121478bd
Jonathan Loescher <jonathan@jloescher.com>
2023-06-19 22:38:14 -0400

    Updated UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,49 +10,50 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex justify-center items-center align-middle my-4">
-        <div className="relative inline-flex items-center">
-          <input
-              type="radio"
-              id="shorten_url"
-              name="option"
-              value="shorten_url"
-              checked={selectedOption === 'shorten_url'}
-              onChange={(e) => setSelectedOption(e.target.value)}
-              className="sr-only"
-          />
-          <label
-              htmlFor="shorten_url"
-              className={`cursor-pointer px-6 py-2 text-sm font-medium rounded-l-lg 
-              ${selectedOption === 'shorten_url' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-          >
-            Shorten URL
-          </label>
-        </div>
-        <div className="relative inline-flex items-center">
-          <input
-              type="radio"
-              id="message_encryption"
-              name="option"
-              value="message_encryption"
-              checked={selectedOption === 'message_encryption'}
-              onChange={(e) => setSelectedOption(e.target.value)}
-              className="sr-only"
-          />
-          <label
-              htmlFor="message_encryption"
-              className={`cursor-pointer px-6 py-2 text-sm font-medium rounded-r-lg 
-              ${selectedOption === 'message_encryption' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-          >
-            Message Encryption
-          </label>
-        </div>
-      </div>
+
       {user ? (
         <div>
-        {selectedOption === 'shorten_url' ?
-            <ShortenUrl user={user} /> : <SecureMessage user={user} />
-        }
+          <div className="flex justify-center items-center align-middle my-4">
+            <div className="relative inline-flex items-center">
+              <input
+                  type="radio"
+                  id="shorten_url"
+                  name="option"
+                  value="shorten_url"
+                  checked={selectedOption === 'shorten_url'}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                  className="sr-only"
+              />
+              <label
+                  htmlFor="shorten_url"
+                  className={`cursor-pointer px-6 py-2 text-sm font-medium rounded-l-lg 
+              ${selectedOption === 'shorten_url' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+              >
+                Shorten URL
+              </label>
+            </div>
+            <div className="relative inline-flex items-center">
+              <input
+                  type="radio"
+                  id="message_encryption"
+                  name="option"
+                  value="message_encryption"
+                  checked={selectedOption === 'message_encryption'}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                  className="sr-only"
+              />
+              <label
+                  htmlFor="message_encryption"
+                  className={`cursor-pointer px-6 py-2 text-sm font-medium rounded-r-lg 
+              ${selectedOption === 'message_encryption' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+              >
+                Message Encryption
+              </label>
+            </div>
+          </div>
+          {selectedOption === 'shorten_url' ?
+              <ShortenUrl user={user} /> : <SecureMessage user={user} />
+          }
         </div>
       ) : (
         <SignIn />
