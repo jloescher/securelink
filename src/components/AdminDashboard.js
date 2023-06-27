0567757993d1f7e778c08a460843069de3fc5827
Jonathan Loescher <jonathan@jloescher.com>
2023-06-27 17:41:12 -0400

    Removed Add user button

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -46,12 +46,7 @@ const AdminDashboard = ({ user, isAdmin, forgotPassword }) => {
                     </p>
                 </div>
                 <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <button
-                        type="button"
-                        className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Add user {/* TODO: Add a model to open user Registration */}
-                    </button>
+
                 </div>
             </div>
             <div className="mt-8 flow-root">
